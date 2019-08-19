b1697a57b9bb5dfb97e116d72e2f4f1630432f52
stephengallo1977 <stephengallo1977@gmail.com>
2019-08-19 17:42:53 -0400

    editedCode

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -6,39 +6,53 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
+router.get("/", function (req, res) {
+  // burger.all(function (data) {
+  //   var hbsObject = {
+  //     burgers: data
+  //   };
+  //   console.log(hbsObject);
+  //   res.render("index", hbsObject);
+  //show all burgers
+  burger.selectAll(function (data) {
+    var BurgersObj = {
       burgers: data
     };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    // console.log(BurgersObj);
+    res.render("index", BurgersObj);
   });
 });
-
-router.post("/", function(req, res) {
-  burger.create([
-    "name", "eaten"
-  ], [
-    req.body.name, req.body.eaten
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+//Allows user to a add a new burger
+// router.post("/", function (req, res) {
+//   burger.create([
+//     "name", "eaten"
+//   ], [
+//     req.body.name, req.body.eaten
+//   ], function (result) {
+  router.post("/api/burgers", function(req,res) {
+    burgers.insertOne(req.body.burger_name, function(result) {
+    // Sends back the ID of the new quote
+    res.json({  id: result.insertId});
+    
   });
 });
-
-router.put("/:id", function(req, res) {
+//allows user to "devour" a burger aka update boolean string for devour
+// router.put("/:id", function (req, res) {
+  router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  burger.update({
-    eaten: req.body.eaten
-  }, condition, function(result) {
+  // burger.update({
+  //   eaten: req.body.eaten
+  // }, condition, function (result) {
+    burgers.updateOne( {devoured: req.body.devoured}, condition, function(result) {
+      //return error if no rows changed
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
-    } else {
+    // } else {
+      
       res.status(200).end();
     }
   });
