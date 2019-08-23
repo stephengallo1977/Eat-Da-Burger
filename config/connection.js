e507d3caa3e7b530b5fe02d7c555f2250668d9d3
stephengallo1977 <stephengallo1977@gmail.com>
2019-08-23 10:40:55 -0400

    editedCode1

--- config/connection.js
+++ config/connection.js
@@ -10,13 +10,13 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+// ;  if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// })
 
 // Export connection for our ORM to use.
 module.exports = connection;
