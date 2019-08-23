3951e26607713e87d4a3933a708721b11c058b10
stephengallo1977 <stephengallo1977@gmail.com>
2019-08-23 09:57:07 -0400

    fixedHeroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
-   connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -16,7 +16,7 @@ if (process.env.JAWSDB_URL) {
     database: "burger"
   });
 
-  // Make connection.
+  // // Make connection.
   // connection.connect(function (err) {
   //   if (err) {
   //     console.error("error connecting: " + err.stack);
@@ -26,5 +26,5 @@ if (process.env.JAWSDB_URL) {
   // });
 
   // Export connection for our ORM to use.
-  module.exports = connection;
 }
+module.exports = connection;
